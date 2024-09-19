5f8bfffc1c6d0066246f1f4db30dba07f3570153
Michael Ajanaku <remiljw@gmail.com>
2024-09-20 00:17:10 +0100

    update iac

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,14 +8,20 @@ terraform {
 
   required_version = "~> 1.9.0"
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "lifi-terraform-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "lifi-terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
   default_tags {
     tags = {
-        owner = "Michael Ajanaku"
-        application = var.application
+      owner       = "Michael Ajanaku"
+      application = var.application
     }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,5 +32,5 @@ variable "ami_id" {
 variable "instance_type" {
   type        = string
   description = "EC2 instance type"
-  default     = "t2.small"
+  default     = "t3.medium"
 }
